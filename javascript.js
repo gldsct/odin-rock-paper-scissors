fe716bca0a27586de327dc09905e86ea88d0e6a6
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-06 18:27:30 +0545

    Implement rock, paper, scissors game logic
    Create a function to implement game logic for a round of rock, paper,
    and scissors. A single round is played, and the winner determined based
    on that sole round.

--- javascript.js
+++ javascript.js
@@ -26,3 +26,57 @@ function getHumanChoice () {
         return "Invalid Choice";
     }
 }
+
+function playRound (humanChoice, computerChoice) {
+    console.log("Human Choice: " + humanChoice);
+    console.log("Computer Choice: " + computerChoice);
+    if (humanChoice === "Rock") {
+        if (computerChoice === "Paper") {
+            console.log("You lose. Paper beats rock.");
+            computerScore++;
+        }
+        else if (computerChoice === "Scissors") {
+            console.log("You win. Rock beats scissors.");
+            humanScore++;
+        }
+        else {
+            console.log("It was a tie.");
+        }
+    }
+    else if (humanChoice === "Paper") {
+        if (computerChoice === "Rock") {
+            console.log("You win. Paper beats rock.");
+            humanScore++;
+        }
+        else if (computerChoice === "Scissors") {
+            console.log("You lose. Scissors beat paper.");
+            computerScore++;
+        }
+        else {
+            console.log("It was a tie.");
+        }
+    }
+    else if (humanChoice === "Scissors") {
+        if (computerChoice === "Rock") {
+            console.log("You lose. Rock beats scissors.");
+            computerScore++;
+        }
+        else if (computerChoice === "Paper") {
+            console.log("You win. Scissors beats paper.");
+            humanScore++;
+        }
+        else {
+            console.log("It was a tie.");
+        }
+    }
+    else {
+        console.log("You picked an invalid choice. Game cannot continue!");
+    }
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
