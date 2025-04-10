0002ad28d0bb22b382ef18a7aeb483c5bb2cfb24
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-10 08:55:39 +0545

    Refactor code to allow starting of new game
    Refactor code to allow the player to start a new game at the end of each
    game, such that players can play a new game without having to refresh
    the webpage otherwise.

--- javascript.js
+++ javascript.js
@@ -70,42 +70,55 @@ function playRound (humanChoice, computerChoice) {
     }
 }
 
-function playGame () {
-    const gameResults = document.createElement("div");
-    gameChoices.appendChild(gameResults);
-    gameChoices.addEventListener("click", (event) => {
-        if (humanScore < 5 && computerScore < 5) {
-            let gameResultsText = document.createElement("p");
-            let roundWinner = playRound(getHumanChoice(event.target.id), getComputerChoice());
-            if (roundWinner === "Human") {
-                humanScore++;
-                gameResultsText.textContent += "You won this round. ";
-            }
-            else if (roundWinner === "Computer") {
-                computerScore++;
-                gameResultsText.textContent += "You lost this round. ";
-            }
-            else {
-                gameResultsText.textContent += "It was a tie. ";
-            }
-            gameResults.appendChild(gameResultsText);
-
-            if (humanScore === 5 || computerScore === 5) {
-                let gameWinner = document.createElement("div");
-                if (humanScore === 5) {
-                    gameWinner.textContent += "You beat the computer! ";
-                }
-                else {
-                    gameWinner.textContent += "The computer bested you. ";
-                }
-                gameChoices.appendChild(gameWinner);
-            }
-        }
-    });
+function endGame () {
+    body.appendChild(newGame);
 }
 
 let humanScore = 0;
 let computerScore = 0;
-const gameChoices = document.querySelector(".game-choices");
 
-playGame();
+const body = document.querySelector("body");
+const gameChoices = document.querySelector(".game-choices");
+const gameResults = document.querySelector(".game-results");
+const gameHistory = document.querySelector("#game-history");
+const gameWinner = document.querySelector("#game-winner");
+
+const newGame = document.createElement("button");
+newGame.id = "new-game";
+newGame.textContent = "Start New Game?";
+
+gameChoices.addEventListener("click", (event) => {
+    if (humanScore < 5 && computerScore < 5) {
+        let roundWinner = playRound(getHumanChoice(event.target.id), getComputerChoice());
+        if (roundWinner === "Human") {
+            humanScore++;
+            gameHistory.textContent += `You won this round.\n`;
+        }
+        else if (roundWinner === "Computer") {
+            computerScore++;
+            gameHistory.textContent += `You lost this round. \n`;
+        }
+        else {
+            gameHistory.textContent += `It was a tie. \n`;
+        }
+
+        if (humanScore === 5 || computerScore === 5) {
+            if (humanScore === 5) {
+                gameWinner.textContent += `\nHuman: ${humanScore} / Computer: ${computerScore} \n\nYou beat the computer!`;
+            }
+            else {
+                gameWinner.textContent += `\nHuman: ${humanScore} / Computer: ${computerScore} \n\nThe computer bested you.`;
+            }
+            endGame();
+        }
+    }
+});
+
+newGame.addEventListener("click", (event) => {
+    event.stopPropagation();
+    humanScore = 0;
+    computerScore = 0;
+    gameHistory.textContent = "";
+    gameWinner.textContent = "";
+    body.removeChild(newGame);
+});
